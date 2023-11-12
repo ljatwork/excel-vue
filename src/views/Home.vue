92e9e5601f3f6a6b1b76352a487b9ed77c7d2a88
Leah Joy Ishiguro <leah_ishiguro@jtekt.co.jp>
2023-11-13 08:53:57 +0900

    Add stepper

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,33 @@
 <template>
   <HeaderComponent @extracted-data="onExcelLoad($event)" />
-  <TransformedTable
-    v-if="jsonToTable"
-    :json-header="jsonToTable[0]"
-    :json-items="jsonToTable.slice(1)"
-  />
+  <v-stepper
+    alt-labels
+    v-model="currentStep"
+    :items="steps"
+    show-actions
+    editable
+  >
+    <template v-slot:item.1>
+      <TransformedTable
+        v-if="jsonToTable"
+        :json-header="jsonToTable[0]"
+        :json-items="jsonToTable.slice(1)"
+      />
+    </template>
+    <template v-slot:item.2>
+      <Models />
+    </template>
+  </v-stepper>
 </template>
 
 <script setup lang="ts">
+import { VStepper } from "vuetify/labs/VStepper";
 import { ref } from "vue";
 import HeaderComponent from "@/components/HeaderComponent.vue";
 import TransformedTable from "@/components/TransformedTable.vue";
 
+const currentStep = ref(1);
+const steps = ref([{ title: "Excel to Json Table" }]);
 const jsonToTable = ref();
 const jsonData = ref();
 
@@ -21,3 +37,10 @@ const onExcelLoad = (eventReturn: any) => {
   console.log(eventReturn.jsonData);
 };
 </script>
+
+<style>
+.v-stepper-item--selected .v-stepper-item__avatar.v-avatar,
+.v-stepper-item--complete .v-stepper-item__avatar.v-avatar {
+  background-color: #e1593b;
+}
+</style>
